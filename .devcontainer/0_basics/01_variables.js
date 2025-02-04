554fb8a81a0738934c3d2d453516f0011ca8e14c
Iramsaba2001 <Kaziiramsaba@gmail.com>
2025-02-04 10:24:11 +0000

    dataTypes

---
+++ .devcontainer/0_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1400990
+let accountEmail = "iramsaba.@gmail.com"
+var accountPassword = "1234567"
+accountCity = "kolhapur"
+let accountState;
+
+
+// accountId = 2   // not allowed
+ accountEmail = "iramsaba52@gmail.com"
+ accountPassword = "12455"
+ accountCity = "pune"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState ]);
---
+++ .devcontainer/0_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use Strict"; // treat all js code as newer version and it can not change because it strictally follow rules;
+//alert(3 + 3) // we are using nodejs not browser that's it cannot print 
+
+console.log(3
+    + 9  
+)// code readability should be high
+
+// types of data types
+
+let name = "iramsaba" //String
+let age = "24" //number
+let isLogged = false // boolean
+
+//Primitive data types
+//number => 2 to power 53 it is a range of number
+//bigint => for long number data
+// String => you can write in '' or ""
+//boolean => true/false
+// null => it is a stand value it is a type also data type //it is also represent of empty value. but null can assigned as a null. null is a object
+// undefind => it mean not usigned value undefind is datatype
+//symbol => to find out uniquness
+
+
+//object=> 
+
+console.log(typeof "iramsaba"); 
+// if you want know which type of variable is then above code is used

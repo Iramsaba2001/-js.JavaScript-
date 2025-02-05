adf8385023fd2dcc489c247d558fd96c225819f6
Iramsaba2001 <Kaziiramsaba@gmail.com>
2025-02-05 00:55:38 +0000

    Operation

--- .devcontainer/0_basics/03_ConversionOpreation.js
+++ .devcontainer/0_basics/03_ConversionOpreation.js
@@ -19,6 +19,39 @@ console.log(typeof score);
 
  
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
+
+
+
+//000000000000000000000000000000000 Operation -------------------------
+ let value = 3
+ let negValue = -value
+ //console.log(negValue);
+ 
+ //if you want to  positive value into a nagtive then above code is used
+let str1 = "hello"
+let str2 = " iramsaba"
+
+let str3 = str1 + str2
+ console.log(str3);
+  // if you want to combine two string it is used
+
+//   console.log("1" + 2);
+//   console.log(1 + "4");
+//   console.log("1" +2 + 2); //if string first then its treat as string
+//   console.log(1 +2 + "2"); //if string last then treat as number
+
+  //its not write code
+
+  let num1, num2, num3
+
+  num1 = num2 = num3 = 2 + 2
+  
+
+  let gameCounter = 100
+  gameCounter++;
+  console.log(gameCounter);
+  
+  
 
 

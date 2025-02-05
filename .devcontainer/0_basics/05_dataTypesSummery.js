289fc801b0487c6a261e11f265c1a7839f376738
Iramsaba2001 <Kaziiramsaba@gmail.com>
2025-02-05 01:55:50 +0000

    datatypesSummery

---
+++ .devcontainer/0_basics/05_dataTypesSummery.js
@@ -0,0 +1,37 @@
+// Primitive data types
+ const score = 100
+ const scorevalue = 100.9
+// example of number datatype
+
+const isLoggedIn = false //boolean
+const outSidetemp = null // null
+// undefind
+let username;
+
+const id= Symbol('1234')
+const anotherid = Symbol('1234')
+
+console.log(id === anotherid);//symbol
+
+const bigNumber = 125485856226315n 
+//it is a bigInt
+
+
+
+//NonPremetive
+const heros = ["shaktiman", "nagraj" , "doga"];
+//Array
+
+let myObj = {
+    name: "iramsaba",
+    age:24
+}
+// its object in object we store any type of data like String int 
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+// function 
+console.log(typeof bigNumber);
+
